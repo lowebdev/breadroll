8197780b813df6e55f089261e1750ac18e1d695c
Olivier Lepage <hello@olivierlepage.dev>
2020-05-08 23:50:40 -0400

    + Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.ts',
+	output: {
+		filename: 'breadroll.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx', '.js' ]
+  }
+};
