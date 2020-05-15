0bbad9d74eb718bad16380c05335ce15988daa72
Olivier Lepage <hello@olivierlepage.dev>
2020-05-14 21:21:36 -0400

    * update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
 	entry: './src/index.ts',
 	output: {
 		filename: 'breadroll.js',
-		path: path.resolve(__dirname, '.dist')
+    path: path.resolve(__dirname, '.dist'),
+    libraryTarget: 'var',
+    libraryExport: 'Breadroll', // Assigns the 'Breadroll' class as the exported object. https://webpack.js.org/configuration/output/#outputlibraryexport
+    globalObject: 'this',
+    library: 'Breadroll' // Name given to the exported object
 	},
   module: {
     rules: [
